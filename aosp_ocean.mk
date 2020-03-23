9f60c6a9bbd0c2edf899d3a5d3265b0eed3bf103
Luiz Dores <luizhenrique.pvh2000@gmail.com>
2020-03-23 14:25:37 +0000

    ocean: Initial pixel bringup

--- aosp_ocean.mk
+++ aosp_ocean.mk
@@ -19,8 +19,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some AEX stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Bootanimation res
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Use Gapps
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
 
 # Inherit from ocean device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,7 +36,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := ocean
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_NAME := lineage_ocean
+PRODUCT_NAME := aosp_ocean
 PRODUCT_MODEL := moto g(7) power
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
